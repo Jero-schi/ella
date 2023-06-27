fc2ef6b3c93bffbe94bf3573bbde5637261bdf38
Jeronimo <jeronimocomaschi05@gmail.com>
2023-06-27 20:49:42 -0300

    siempre me olvido de agregar los nuevos archivos

---
+++ info.js
@@ -0,0 +1,4 @@
+const info = [
+    { id: 1, fecha: '22-12-19', img: './assets/IMG-20191227-WA0032.jpg', info: 'Esa magica noche, ese abrazo inolvidable...', position: '0% 35%' },
+    { id: 2, fecha: '16-12-22', img: './assets/IMG-20221218-WA0055 (1).jpg', info: 'Un recuerdo para toda la vida', position: '0% 20%' }
+]
