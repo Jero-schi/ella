f75f10ee5485e6cab2c2695bde5a2b83ef84c731
Jeronimo <jeronimocomaschi05@gmail.com>
2023-06-28 17:18:50 -0300

    dias entre fecha y fecha

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const linea = document.querySelector('.linea')
 const body =  document.querySelector('body')
+const dia =  document.querySelector('#dias')
 const section =  document.querySelector('section')
 
-info.forEach(fecha => {
-    // console.log(fecha);
+info.forEach((fecha, index) => {
+    // console.log(index);
     section.innerHTML += `
-    <div class="conteiner">
+    <div id="conteiner-${index}" class="conteiner">
     <p class="fecha">${fecha.fecha}</p>
 
     <div class="conteiner-img">
@@ -36,3 +37,14 @@ const observador =  new IntersectionObserver(animarBarrita, {
 })
 
 observador.observe(imgs[0])
+
+
+let hoy = Date.now()
+let fecha1 = new Date("07/23/2019");
+let fecha2 = new Date(hoy);
+
+let diferencia = fecha2.getTime() - fecha1.getTime();
+
+let diasDeDiferencia = Math.trunc(diferencia / 1000 / 60 / 60 / 24);
+
+dia.textContent = `${diasDeDiferencia} días`
