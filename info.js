4e1dc982aa1909c98d1f0924cbae679d097fb818
Jeronimo <jeronimocomaschi05@gmail.com>
2023-06-27 22:34:56 -0300

    optimizando imagenes

--- info.js
+++ info.js
@@ -1,15 +1,15 @@
 const position = '0% 20%'
 
 const info = [
-    { id: 0, fecha: '24-06-19', img: './assets/IMG_20190624_171224409.jpg', info: 'Cuando se empezaba a formar un nosotros', position: position },
-    { id: 1, fecha: '22-12-19', img: './assets/IMG-20191227-WA0032.jpg', info: 'Esa magica noche, ese abrazo inolvidable...', position: '0% 35%' },
-    { id: 2, fecha: '16-12-22', img: './assets/Pa subir (1).jpg', info: 'Se podria decir que el atardecer fue lo mas hermoso que vio mis ojos esa tarde. Pero no, fuiste vos', position: '0% 50%' },
-    { id: 3, fecha: '05-09-21', img: './assets/IMG-20210905-WA0039.jpg', info: 'Una de nuestras tardes, donde no existia nada mas que nosotros dos', position: '0% 0%' },
-    { id: 4, fecha: '16-10-21', img: './assets/IMG-20211016-WA0002 (1).jpg', info: 'Besos, besos y mas besos', position: '0% 15%' },
-    { id: 5, fecha: '05-11-21', img: './assets/IMG-20211105-WA0095.jpg', info: 'La noche que pase mas calor en mi vida, pero sí, valio la pena, con tal de verte feliz', position: '0% 80%' },
-    { id: 6, fecha: '02-03-22', img: './assets/IMG-20220302-WA0082.jpg', info: 'El comienzo de un año que no voy a olvidar nunca en la vida', position: '0% 50%' },
-    { id: 7, fecha: '19-09-22', img: './assets/IMG-20220919-WA0039.jpg', info: 'Actores...', position: '0% 10%' },
-    { id: 8, fecha: '02-11-22', img: './assets/IMG-20221102-WA0001.jpg', info: 'Acompañandote, como siempre voy a hacerlo', position: '70% 0%' },
-    { id: 9, fecha: '14-12-22', img: './assets/IMG-20221214-WA0067 (2).jpg', info: 'Quizas nuestra mejor foto juntos', position: position },
-    { id: 10, fecha: '16-12-22', img: './assets/IMG-20221218-WA0055 (1).jpg', info: 'Un recuerdo para toda la vida', position: '0% 10%' },
+    { id: 0, fecha: '24-06-19', img: './assets/IMG_20190624_171224409.webp', info: 'Cuando se empezaba a formar un nosotros', position: position },
+    { id: 1, fecha: '22-12-19', img: './assets/IMG-20191227-WA0032.webp', info: 'Esa magica noche, ese abrazo inolvidable...', position: '0% 35%' },
+    { id: 2, fecha: '16-12-22', img: './assets/Pa subir (1).webp', info: 'Se podria decir que el atardecer fue lo mas hermoso que vio mis ojos esa tarde. Pero no, fuiste vos', position: '0% 50%' },
+    { id: 3, fecha: '05-09-21', img: './assets/IMG-20210905-WA0039.webp', info: 'Una de nuestras tardes, donde no existia nada mas que nosotros dos', position: '0% 0%' },
+    { id: 4, fecha: '16-10-21', img: './assets/IMG-20211016-WA0002 (1).webp', info: 'Besos, besos y mas besos', position: '0% 15%' },
+    { id: 5, fecha: '05-11-21', img: './assets/IMG-20211105-WA0095.webp', info: 'La noche que pase mas calor en mi vida, pero sí, valio la pena, con tal de verte feliz', position: '0% 80%' },
+    { id: 6, fecha: '02-03-22', img: './assets/IMG-20220302-WA0082.webp', info: 'El comienzo de un año que no voy a olvidar nunca en la vida', position: '0% 50%' },
+    { id: 7, fecha: '19-09-22', img: './assets/IMG-20220919-WA0039.webp', info: 'Actores...', position: '0% 10%' },
+    { id: 8, fecha: '02-11-22', img: './assets/IMG-20221102-WA0001.webp', info: 'Acompañandote, como siempre voy a hacerlo', position: '70% 0%' },
+    { id: 9, fecha: '14-12-22', img: './assets/IMG-20221214-WA0067 (2).webp', info: 'Quizas nuestra mejor foto juntos', position: position },
+    { id: 10, fecha: '16-12-22', img: './assets/IMG-20221218-WA0055.webp', info: 'Un recuerdo para toda la vida', position: '0% 10%' },
 ]
